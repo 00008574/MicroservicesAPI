a1f4cfb89b5305abe8d2e25e9ef1e61daeef6504
unknown <00008574wiut@gmial.com>
2021-11-04 13:33:21 +0500

    added Models folder. Product and Category models created

--- CW1-MicroservicesAPI-8574/Models/Category.cs
+++ CW1-MicroservicesAPI-8574/Models/Category.cs
@@ -7,5 +7,8 @@ namespace CW1_MicroservicesAPI_8574.Models
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
--- CW1-MicroservicesAPI-8574/Models/Product.cs
+++ CW1-MicroservicesAPI-8574/Models/Product.cs
@@ -7,5 +7,10 @@ namespace CW1_MicroservicesAPI_8574.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category ProductCategory { get; set; }
+        public decimal Price { get; set; }
     }
 }
