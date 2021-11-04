c08575fae893e42c0f40f090054fd44ef6ab694d
unknown <00008574wiut@gmial.com>
2021-11-04 15:06:16 +0500

    added ProductContext and added connection string in appsettings.json file

--- CW1-MicroservicesAPI-8574/DBContexts/ProductContext.cs
+++ CW1-MicroservicesAPI-8574/DBContexts/ProductContext.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using CW1_MicroservicesAPI_8574.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CW1_MicroservicesAPI_8574.DBContexts
 {
-    public class ProductContext
+    public class ProductContext : DbContext
     {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
